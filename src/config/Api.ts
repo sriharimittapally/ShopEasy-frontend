4f493d10d47fe011e5fa08822251e39e92529389
sriharimittapally <sriharimittapally95@gmail.com>
2025-02-27 15:02:07 +0530

    Update Api.ts

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "https://shopeasy-ecommerce-multivendor-production.up.railway.app";
+export const API_URL = "https://ecommerce-multivendor-backend-production.up.railway.app";
 
 export const api = axios.create({
   baseURL: API_URL,
